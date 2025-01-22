60e18273b985bde31a654c54fe33688786f6b8b8
Mohammad Rauf <mohammadrauf785@gmail.com>
2025-01-22 12:58:28 +0300

    added disabling property on minus button

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,46 +1,49 @@
 import { RoundedButton_V2 } from '@/components/RoundButton';
-import { FontAwesomeIcon } from '@/components/Themed';
-import { IconProp } from '@fortawesome/fontawesome-svg-core';
 import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, TextInput, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 
-
 export default function TabTwoScreen() {
-  const [number, setNumber] = useState('0');
+  const [number, setNumber] = useState('1');
 
   const plus = (a: number) => {
     const add: number = parseInt(number) + a;
     setNumber(add.toString())
-    console.log("I am gonna add");
+    // console.log("I am gonna add");
   }
 
-  const minus = (a: number) => {
-    const minus: number = parseInt(number) - a;
-    setNumber(minus.toString())
-    console.log("I am gonna subtract");
-  }
-
+  const minus = (a: number) =>  {
+    if (number > '0') {
+        const minus: number = parseInt(number) - a;
+        setNumber(minus.toString())
+        // console.log("I am gonna subtract");
+      }
+    }
 
 
   return (
-    <SafeAreaView className='flex-1 items-center justify-center bg-primary-100'>
-      <Text className='text-xl text-red-400'>Select the number of dice(s) you wish to play with</Text>
+    <SafeAreaView className='items-center justify-center bg-primary-100'>
+      <Text className='text-xl text-primary-500'>Set the number of field(s) you wish to play with</Text>
       <View className="flex-row flex items-center justify-center gap-4">
-        <RoundedButton_V2 icon={['fas', 'plus']} onPress={(x) => plus(x)} value={1} />
+        
+      <RoundedButton_V2 icon={['fas', 'minus']} onPress={minus} value={1} disabled={number === '1'}/>
+
         <TextInput
           className='text-4xl bg-transparent p-4 rounded-lg w-20 h-20 flex items-center justify-center text-center'
-
+          placeholder="Enter a number"
           onChangeText={setNumber}
           value={number}
-          keyboardType='numeric'
-          readOnly
+          keyboardType="numeric"
+          // readOnly // Make the input field read-only
         />
-        <RoundedButton_V2 icon={['fas', 'minus']} onPress={(x) => minus(x)} value={1} />
+
+        <RoundedButton_V2 icon={['fas', 'plus']} onPress={plus} value={1} disabled={false} />
       </View>
+
+      {/* <RoundedButton_V2  placeholder="Generate Field(s)"/> */}
 
 
     </SafeAreaView>
-  );
+  ); 
 }
--- components/RoundButton.tsx
+++ components/RoundButton.tsx
@@ -12,10 +12,26 @@ interface RoundedButton_V2Props {
     icon: IconProp; 
     onPress: (a: number) => void; 
     value: number;
+    disabled: boolean
 }
 
-export const RoundedButton_V2 = ({ icon, onPress, value }: RoundedButton_V2Props) => (
-    <TouchableOpacity onPress={() => onPress(value)}  style={{ width: 30, height: 30, backgroundColor: '#00000' }}>
+export const RoundedButton_V2 = ({ icon, onPress, value, disabled }: RoundedButton_V2Props) => (
+    <TouchableOpacity onPress={() => onPress(value)}  
+        style={{ width: 30, 
+                height: 30, 
+                // backgroundColor: '#00000', 
+                borderWidth: 1, // Add border width
+                borderColor: '#00000', // Add border color
+                borderTopLeftRadius: 10, // Half of the width and height to make it rounded
+                borderBottomRightRadius: 10,
+                borderLeftColor: '#FFFFF',
+                alignItems: 'center',
+                justifyContent: 'center', 
+                opacity: disabled ? 0.2 : 1, // Reduce opacity when disabled
+        }} 
+        disabled={disabled}
+        >
+            
         <FontAwesomeIcon icon={icon} size={26} color='#000000' />
     </TouchableOpacity>
 
